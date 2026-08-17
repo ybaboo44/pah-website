18ed5fbead93031e79656c9c1dae0240e202094e
steeve44 <112339292+ybaboo44@users.noreply.github.com>
2026-08-17 16:58:17 -0400

    Update next-env.d.ts with PostgreSQL connection string
    Replaced reference types line with PostgreSQL connection string.

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="next" />
+postgresql://neondb_owner:npg_fbnJh8Mr4oZd@ep-twilight-forest-axfmfs9w-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require/// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
